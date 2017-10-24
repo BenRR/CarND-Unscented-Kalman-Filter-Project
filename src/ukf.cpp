a22bc173763d37193d9a648724f67f064a3d3bac
Ben Kuai <ben.kuai@team.telstra.com.au>
2017-10-25 08:50:02 +1100

    tune params

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -23,12 +23,11 @@ UKF::UKF() {
   // initial covariance matrix
   P_ = MatrixXd(5, 5);
 
-  //TODO tune noise param
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 2;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.5;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
